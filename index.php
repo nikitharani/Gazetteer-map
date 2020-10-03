5c8e7ca0fcabbae91debd41f072370422ff5b8e7
Nikitha <nikithanalumachu@gmail.com>
2020-10-04 00:31:35 +0100

    database schedule working

--- index.php
+++ index.php
@@ -20,12 +20,19 @@
   }
 
   // send entries to database function  
-  function sendDb_data($sql_connection, $table,$apiData){
-    delDb_data($sql_connection, $table);
+  function sendDb_data($sql_connection, $table, $apiData){
+    global $currency_code;
+    $query = '';
+    $currencyValue = 0;
     $currency_object = $apiData['rates'];//json_decode($data, true); //Convert JSON String into PHP Array
+    $time_stamp = $apiData['timestamp'];
     foreach($currency_object as $key => $value) //Extract the Array Values by using Foreach Loop
     {          
-      $query .= "INSERT INTO $table (currency_code , currency_rate) VALUES ('$key', $value);";         
+      $query .= "INSERT INTO $table (time_stamp, currency_code , currency_rate) VALUES ($time_stamp, '$key', $value);";  
+      if (!strcmp($key,$currency_code))
+          {
+            $currencyValue = $value;
+          }       
     }    
     
     if(mysqli_multi_query($sql_connection, $query)) //Run Mutliple Insert Query
@@ -33,25 +40,29 @@
       global $display_echo;
       writeMsg("employee data inserted",$display_echo);      
     }
+    return $currencyValue;
 }
 
 function getDb_data($sql_connection, $table, $currency_code)
     {
-      $retrive ="SELECT currency_code, currency_rate FROM $table";
+      $retrive ="SELECT time_stamp, currency_code, currency_rate FROM $table";
       $curr_result = mysqli_query($sql_connection, $retrive);
       $currencyValue = 0;
       global $display_echo;
+      global $time_database;
 
       if (mysqli_num_rows($curr_result) > 0) {
         // output data of each row
         while($row = mysqli_fetch_assoc($curr_result)) {
           // echo "currency_code: " . $row["currency_code"]. " - currency_rate: " . $row["currency_rate"]. "<br>";
+          $time_database = $row["time_stamp"];
           if (!strcmp($row["currency_code"],$currency_code))
           {
             $currencyValue = $row["currency_rate"];
             break;
           }
         }
+        writeMsg("Found entries in database",$display_echo);
       } else {
         writeMsg("No entries in database",$display_echo);      
       }
@@ -59,78 +70,62 @@ function getDb_data($sql_connection, $table, $currency_code)
       return $currencyValue;
   }
 
+  function getTimeDiffInMins($current_dateTime,$previous_dateTime)
+  {
+    //--------------------------------------------------------------//
+    // Find the time difference b/w current & previous time request//
+    //--------------------------------------------------------------//
+
+    $start_date = new DateTime($current_dateTime);
+    $since_start = $start_date->diff(new DateTime($previous_dateTime));
+  
+    //To get the total number of minutes:
+    $minutes = $since_start->days * 24 * 60;
+    $minutes += $since_start->h * 60;
+    $minutes += $since_start->i;
+    return $minutes;
+  }
+
+   // Get currency json from API
+   function getCurrencyApi(){
+    $url='https://openexchangerates.org/api/latest.json?app_id=0f69fa8292e147f7ba4e3d02a4fa52f0';    
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_URL,$url);
+
+    $result=curl_exec($ch);
+
+    curl_close($ch);
+
+    $decode = json_decode($result,true);	
+    return $decode;
+
+   }
+
   // global variables here
   $display_echo = false;
   $table_name = "my_table";
   $myFile = "curr_data.json";
+  $useApiData=true;
+  $current_dateTime = gmdate("Y-m-d H:i:s");
+  $time_database = 0;
 
-  // Find the time difference b/w current & previous time request
-  /*$current_dateTime = date("Y-m-d h:i:sa");
-  $previous_dateTime; 
-  
- 
-  $minutes =0;
-  echo 'currentTime :'. ($current_dateTime);
-  // echo 'previousTime :'. $previous_dateTime;
-
-  if (!empty($previous_dateTime))
-  {
-  $start_date = new DateTime($current_dateTime);
-  $since_start = $start_date->diff($previous_dateTime);
-
-//To get the total number of minutes:
-$minutes = $since_start->days * 24 * 60;
-$minutes += $since_start->h * 60;
-$minutes += $since_start->i;
-  }
-// echo $minutes.' minutes';
-
-if(($minutes>0.5) || (empty($previous_dateTime))) {
-  echo 'writing data to db';
-}else{
-echo 'using old db';
-}
-$previous_dateTime=$current_dateTime;*/
-
-
-
-
-
-    $executionStartTime = microtime(true) / 1000;
-    if (empty($_REQUEST['curr_code']))
-    {$currency_code = "AUD";}
-    else {$currency_code = $_REQUEST['curr_code'];}
-
-    // // Get currency json from API
-    // $url='https://openexchangerates.org/api/latest.json?app_id=0f69fa8292e147f7ba4e3d02a4fa52f0';    
-
-    // $ch = curl_init($url);
-    // curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    // curl_setopt($ch, CURLOPT_URL,$url);
-
-    // $result=curl_exec($ch);
-
-    // curl_close($ch);
-
-    // $decode = json_decode($result,true);	
+  $executionStartTime = microtime(true) / 1000;
+  if (empty($_REQUEST['curr_code']))
+  {$currency_code = "AUD";}
+  else {$currency_code = $_REQUEST['curr_code'];}
 
     // // write json to a file
     // if(file_put_contents($myFile, $result))
     //  {
     //   writeMsg('Data successfully saved to Json file',$display_echo);
     //  }
-    // else {writeMsg('error in saving data to json file',$display_echo);}
-      
+    // else {writeMsg('error in saving data to json file',$display_echo);}      
 
-    //Get data from existing json file
-    $result = file_get_contents($myFile);
-
-    $output['status']['code'] = "200";
-    $output['status']['name'] = "ok";
-    $output['status']['description'] = "Currency information";
-    $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
-
+    // //Get data from existing json file
+    // $result = file_get_contents($myFile);
 
     //connection to Database
     $connect = mysqli_connect("localhost", "root", "", "currency_db"); //Connect PHP to MySQL Database
@@ -139,26 +134,22 @@ $previous_dateTime=$current_dateTime;*/
     }
     writeMsg("connection established",$display_echo);
 
-    $query = '';
-    $table_data = '';
-
-    //  Delete or update old data
-    // delDb_data($connect, $table_name);    
-    
-
-    // // send entries to database
-    // $milliseconds = 5000;
-    // $seconds=(int)$milliseconds/1000;
-    // while(true)
-    // {
-    //   sendDb_data($connect, $table_name,$decode);
-    //   sleep($seconds);
-    // }
-    
-
     // retrive data from database
     $output['data'] = getDb_data($connect, $table_name, $currency_code);  
+    $previous_dateTime = gmdate("Y-m-d H:i:s", $time_database);
+    $minutes = getTimeDiffInMins($current_dateTime,$previous_dateTime);
+    writeMsg("minutes: $minutes",$display_echo);
+    writeMsg("current_dateTime: $current_dateTime",$display_echo);
+    writeMsg("previous_dateTime: $previous_dateTime",$display_echo);
 
+    if($minutes>180){
+      //get currency using Api
+      $decode=getCurrencyApi();
+      //  Delete or update old data
+      delDb_data($connect, $table_name); 
+      // send entries to database
+      $output['data']=sendDb_data($connect, $table_name,$decode);
+    }  
 
     // close sql connection
       $connect_close  = mysqli_close($connect);
@@ -171,6 +162,10 @@ $previous_dateTime=$current_dateTime;*/
       }
 
       // output for ajax
+      $output['status']['code'] = "200";
+      $output['status']['name'] = "ok";
+      $output['status']['description'] = "Currency information";
+      $output['status']['returnedIn'] = (microtime(true) - $executionStartTime) / 1000 . " ms";
       header('Content-Type: application/json; charset=UTF-8');    
       echo json_encode($output); 
 
